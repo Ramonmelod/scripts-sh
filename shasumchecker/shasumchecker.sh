8a8fc3e71a4f8567a1126c761772b8951066a70d
Ramonmelod <ramonmelo.com@gmail.com>
2024-05-29 12:20:35 -0300

    initial implementation of the check of the file sha256sum

---
+++ shasumchecker/shasumchecker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+text=$(awk '{ print $1 }' bash-logo.txt) # returns the first line block
+
+sha256sum=$(sha256sum bash-logo.jpg | awk '{print $1}') # this pipe's propose is to send only the first line block
+echo $sha256sum
+echo $text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
