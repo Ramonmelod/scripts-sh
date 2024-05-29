070ad0d14cbe1911c9368a162c60e41cedab3ce4
Ramonmelod <ramonmelo.com@gmail.com>
2024-05-29 13:35:18 -0300

    implementation of the if statement

--- shasumchecker/shasumchecker.sh
+++ shasumchecker/shasumchecker.sh
@@ -3,11 +3,15 @@
 text=$(awk '{ print $1 }' bash-logo.txt) # returns the first line block
 
 sha256sum=$(sha256sum bash-icon.svg | awk '{print $1}') # this pipe's propose is to send only the first line block
-echo $sha256sum
-echo $text
 
+if [ "$sha256sum" == "$text" ]; then
 
+echo "O arquivo é original!"
+else
 
+echo "O arquivo não é original!"
+
+fi
 
 
 
