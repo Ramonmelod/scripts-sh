8506011461ac2ada8e1b0291d8bb076e7efc513a
Ramonmelod <ramonmelo.com@gmail.com>
2024-05-29 13:05:12 -0300

    change of the bash-logo file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Scripts shell
+<img src="https://www.svgrepo.com/show/353478/bash-icon.svg" alt="logo Neovim" width="200" height="auto">
 
 ## Here you find some samples of shell scripts
--- shasumchecker/shasumchecker.sh
+++ shasumchecker/shasumchecker.sh
@@ -2,7 +2,7 @@
 
 text=$(awk '{ print $1 }' bash-logo.txt) # returns the first line block
 
-sha256sum=$(sha256sum bash-logo.jpg | awk '{print $1}') # this pipe's propose is to send only the first line block
+sha256sum=$(sha256sum bash-icon.svg | awk '{print $1}') # this pipe's propose is to send only the first line block
 echo $sha256sum
 echo $text
 
