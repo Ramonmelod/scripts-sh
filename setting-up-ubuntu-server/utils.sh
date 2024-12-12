9d53c2d5fc71603180be3c9f901271a3caeaf336
Ramon Melo <ramonmelo.com@gmail.com>
2024-12-12 17:29:45 -0300

    feat: implements the command source .baschrc in order to restart bash

--- setting-up-ubuntu-server/utils.sh
+++ setting-up-ubuntu-server/utils.sh
@@ -8,3 +8,4 @@ echo "installing asdf"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
 echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
 echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+source .bashrc
