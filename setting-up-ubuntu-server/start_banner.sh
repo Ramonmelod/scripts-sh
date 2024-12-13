83df4e19f3e296471a4203bf5f231e3f699cf69d
Ramonmelod <ramonmelo.com@gmail.com>
2024-12-13 13:03:15 -0300

    feat: adds a pipe and the `tee` to start_banner.txt

--- setting-up-ubuntu-server/start_banner.sh
+++ setting-up-ubuntu-server/start_banner.sh
@@ -4,6 +4,4 @@ echo "start_banner.sh running"
 
 path=$(pwd)
 
-sudo echo "Banner ${path}/ssh_banner.txt"  >> /etc/ssh/sshd_config
-
-
+echo "Banner ${path}/ssh_banner.txt" | sudo tee -a /etc/ssh/sshd_config
