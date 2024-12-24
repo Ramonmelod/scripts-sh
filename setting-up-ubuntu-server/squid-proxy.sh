d5f00b13d666526201e4f283ec8485ab994444fd
Ramonmelod <ramonmelo.com@gmail.com>
2024-12-24 13:30:24 -0300

    feat: adds terminal messages of the current state of the script for the user reference

--- setting-up-ubuntu-server/squid-proxy.sh
+++ setting-up-ubuntu-server/squid-proxy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-sudo apt install squid 
+sudo apt install squid -y
 file_name="squid.conf"
 dir_path=$(pwd)
 
 file_path=${dir_path}/${file_name}
 
 # makes a copy of the original squid.conf file 
+echo "making a copy of the original squid.conf file, in case there is any"
 sudo cp /etc/squid/squid.conf /etc/squid/squid.conf.original 
+
+echo "removing the original squid.con file, in case there is any"
 sudo rm /etc/squid/squid.conf
-cp $file_path /etc/squid
+
+echo "making a copy of the squid.conf file of this repository to the /etc/squid"
+sudo cp $file_path /etc/squid
+
+echo "restarting the squid.service in order to source the new squid.conf file "
 sudo systemctl restart squid
+
+echo "calling the status of the squid service daemon"
 sudo systemctl status squid
 
