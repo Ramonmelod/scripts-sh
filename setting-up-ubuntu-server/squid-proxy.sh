7c925751838d5d801e74dbc55a6d999ace791bfc
Ramonmelod <ramonmelo.com@gmail.com>
2024-12-24 13:38:00 -0300

    feat:adds \n to give a space between the echo messages

--- setting-up-ubuntu-server/squid-proxy.sh
+++ setting-up-ubuntu-server/squid-proxy.sh
@@ -7,18 +7,17 @@ dir_path=$(pwd)
 file_path=${dir_path}/${file_name}
 
 # makes a copy of the original squid.conf file 
-echo "making a copy of the original squid.conf file, in case there is any"
+echo -e "making a copy of the original squid.conf file, in case there is any\n"
 sudo cp /etc/squid/squid.conf /etc/squid/squid.conf.original 
 
-echo "removing the original squid.con file, in case there is any"
+echo -e "removing the original squid.con file, in case there is any\n"
 sudo rm /etc/squid/squid.conf
 
-echo "making a copy of the squid.conf file of this repository to the /etc/squid"
+echo -e "making a copy of the squid.conf file of this repository to the /etc/squid\n"
 sudo cp $file_path /etc/squid
 
-echo "restarting the squid.service in order to source the new squid.conf file "
+echo -e "restarting the squid.service in order to source the new squid.conf file\n"
 sudo systemctl restart squid
 
-echo "calling the status of the squid service daemon"
+echo -e "calling the status of the squid service daemon\n"
 sudo systemctl status squid
-
