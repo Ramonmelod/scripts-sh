33c46e37598be7d35ec0dabbb7456e326a733de6
Ramonmelod <ramonmelo.com@gmail.com>
2024-12-24 14:24:14 -0300

    feat: adds all the echo information for a better terminal user experience

--- setting-up-ubuntu-server/main.sh
+++ setting-up-ubuntu-server/main.sh
@@ -4,6 +4,12 @@ sudo apt update && sudo apt upgrade -y
 
 echo -e " Running the util.sh script in order to install usefull packages like:\n asdf \n chromium \n chromium driver and others\n "
 ./utils.sh 
+
+echo -e " Running the docker-install.sh script in order to install docker\n"
 ./docker-install.sh 
+
+echo -e " Running the start_banner.sh script in order to set the start banner server\n\n #####\n\n\n Do not forget to change this project banner for your own banner!\n\n\n ####\n\n\n "
 ./start_banner.sh
+
+echo -e " Running the squid-proxy script in order to install squid and make available your personal proxy server\n "
 ./squid-proxy
