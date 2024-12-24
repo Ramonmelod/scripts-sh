e33c68dc8e3a923f4729994b5dff6a67f8d8e362
Ramonmelod <ramonmelo.com@gmail.com>
2024-12-24 18:16:42 -0300

    feat: adds the call of the script nvim-setup.sh to the main.sh script

--- setting-up-ubuntu-server/main.sh
+++ setting-up-ubuntu-server/main.sh
@@ -2,14 +2,17 @@
 
 sudo apt update && sudo apt upgrade -y
 
-echo -e " \n\n\nRunning the util.sh script in order to install usefull packages like:\n asdf \n chromium \n chromium driver and others\n "
+echo -e " \n\n\nRunning the util.sh script in order to install usefull packages like:\n asdf \n chromium \n chromium driver and others\n\n\n "
 ./utils.sh 
 
 echo -e " Running the start_banner.sh script in order to set the start banner server\n\n #####\n\n\n Do not forget to change this project banner for your own banner!\n\n\n ####\n\n\n "
 ./start_banner.sh
 
-echo -e " Running the squid-proxy script in order to install squid and make available your personal proxy server\n "
+echo -e " Running the squid-proxy script in order to install squid and make available your personal proxy server\n\n\n "
 ./squid-proxy
+
+echo -e " Running the nvim-setup.sh script in order to download Ramon Melo Neovim setup\n\n\n "
+./nvim-setup.sh 
 
 echo -e " Running the docker-install.sh script in order to install docker\n"
 ./docker-install.sh 
